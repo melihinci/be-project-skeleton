9d9fb604ad208cd7d5361856e77f38cac303f822
Melih İNCİ <melihinci@hotmail.com.tr>
2024-07-17 03:13:19 +0300

    auth developing

--- AuthService/src/main/java/com/melihinci/skeleton/advice/LoggingInterceptor.java
+++ AuthService/src/main/java/com/melihinci/skeleton/advice/LoggingInterceptor.java
@@ -1,27 +1,63 @@
 package com.melihinci.skeleton.advice;
 
 import org.apache.logging.log4j.ThreadContext;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class LoggingInterceptor implements HandlerInterceptor {
 
+
+    private final String pod_id = UUID.randomUUID().toString();
+    private final Environment env;
+    private final String appName;
+    private final String pod_version;
+
+    public LoggingInterceptor(Environment env) {
+        this.env = env;
+        appName = this.env.getProperty("spring.application.name");
+        pod_version = this.env.getProperty("spring.application.version");
+    }
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        String traceId = UUID.randomUUID().toString();
-        ThreadContext.put("trace_id", traceId);
 
-        String threadId = String.valueOf(Thread.currentThread().getId());
-        ThreadContext.put("thread_id", threadId);
-
+        if (request.getHeader("X-Trace-Id") != null) {
+            ThreadContext.put("trace_id", request.getHeader("X-Trace-Id"));
+        } else {
+            ThreadContext.put("trace_id", UUID.randomUUID().toString());
+        }
+        ThreadContext.put("thread_id", String.valueOf(Thread.currentThread().getId()));
+        ThreadContext.put("authToken", maskAuthToken(request));
+        ThreadContext.put("pod_id", pod_id);
+        ThreadContext.put("pod_name", appName);
+        ThreadContext.put("pod_version", "1.0.0"); //TODO: get version from JAR MANIFEST
+        ThreadContext.put("request_type", request.getMethod());
+        ThreadContext.put("request_url", request.getRequestURI() + "?" + request.getQueryString());
+        try {
+            ThreadContext.put("request_body", request.getReader().lines().collect(Collectors.joining(System.lineSeparator())));
+        } catch (Exception e) {
+            ThreadContext.put("request_body", "");
+        }
         return true;
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
         ThreadContext.clearAll();
+    }
+
+    private String maskAuthToken(HttpServletRequest request) {
+        String authToken = Optional.ofNullable(request.getHeader("X-Auth-Token")).orElse("00000000-0000-0000-0000-000000000000");
+        int length = authToken.length();
+        if (length <= 6) {
+            return authToken;
+        }
+        return authToken.substring(0, 3) + "****" + authToken.substring(length - 3);
     }
 }
---
+++ AuthService/src/main/java/com/melihinci/skeleton/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.melihinci.skeleton.config;
+
+import com.melihinci.skeleton.advice.LoggingInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final Environment env;
+
+    public WebConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggingInterceptor(env));
+    }
+}
